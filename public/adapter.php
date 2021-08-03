f077e0a2f6aef62bac15733adcce3b841b9ed115
Gary Clarke <garyclarke1974@gmail.com>
2021-08-03 10:58:53 +0100

    Revert for demo images

--- public/adapter.php
+++ public/adapter.php
@@ -2,9 +2,7 @@
 
 require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
 
-$newCsvFileWriter = new \App\NewCsvFileWriter();
-
-$fileWriter = new \App\NewFileWriterAdapter($newCsvFileWriter);
+$fileWriter = new \App\NewCsvFileWriter();
 
 $processor = new \App\RandomProcessor($fileWriter);
 $processor->process(['foo' => 'bar']);
